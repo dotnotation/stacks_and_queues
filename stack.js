0c5bdde0900772f39570d28c5cb149c19aa7a965
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-17 14:57:07 -0500

    adding pseudocode

--- stack.js
+++ stack.js
@@ -11,4 +11,13 @@ class Stack {
         this.last = null
         this.size = 0
     }
+
+    push(val){
+        // accepts a value and creates a new node with that value
+        // if there are no nodes, set the first and last to be the new node
+        // if there is at least one node, create a variable that stores the current first property on the stack
+        // reset the first property to be the new node
+        // set the next property to be the previously created variable
+        // increment the size of the stack by 1
+    }
 }
