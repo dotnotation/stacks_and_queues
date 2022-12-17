2e3ae420f656abf9901ad7ab8b2f06ab78de8aa2
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-17 16:25:07 -0500

    solution for dequeue

--- queues.js
+++ queues.js
@@ -13,6 +13,7 @@ class Queue {
     }
 
     enqueue(val){
+        // adding to the end 
         // create a new node with val
         // if no nodes, set the new node node to be first and last property
         // else set the next property on the current last to be the new node
@@ -27,5 +28,27 @@ class Queue {
             this.last = newNode
         }
         return ++this.size
+    }
+
+    dequeue(){
+        // remove from the beginning
+        // if no first property return null
+        // store the first property in a variable
+        // see if first === last or size === 1, if so then set first and last to be null
+        // else set the first property to be the next property of the first
+        // decrease the sie by 1 
+        // return the value of the node dequeued 
+
+        if (!this.first) return null
+        
+        let firstNode = this.first 
+
+        if (this.first === this.last){
+            this.first = null
+            this.last = null
+        }
+        this.first = this.first.next 
+        this.size--
+        return firstNode.val
     }
 }
